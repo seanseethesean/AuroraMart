a4cd139320bcfb1fba6b5c4b1f3be883306450b3
eleanorcheak <155040893+eleanorcheak@users.noreply.github.com>
2025-10-26 22:47:10 +0800

    css

--- auroramartproj/auroramartproj/settings.py
+++ auroramartproj/auroramartproj/settings.py
@@ -56,13 +56,14 @@ ROOT_URLCONF = 'auroramartproj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.debug',
             ],
         },
     },
